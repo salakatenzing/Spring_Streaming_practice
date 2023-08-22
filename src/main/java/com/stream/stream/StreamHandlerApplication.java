d196aff7040cdf7691e09528072648340ce32944
Tenzing Salaka <salakatenzing@gmail.com>
2023-08-21 23:29:35 -0400

    initial setup for Spring Cloud Stream use

---
+++ src/main/java/com/stream/stream/StreamHandlerApplication.java
@@ -0,0 +1,13 @@
+package com.stream.stream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamHandlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamHandlerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stream/stream/StreamHandlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stream.stream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamHandlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
